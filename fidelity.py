abe2ebaf5ebc7165a570d77d723a867429b2af99
bdemcniven <71337222+bdemcniven@users.noreply.github.com>
2025-02-24 11:02:23 -0330

    Add files via upload

--- fidelity.py
+++ fidelity.py
@@ -1,12 +1,11 @@
 from pkg_req import *
 import numpy as np
 
-def calculate_fidelity(counts, ideal_density_matrix):
-
+def fidelity_x0(x0, counts, ideal_density_matrix):
+    x0 = [100,1,1,1]
 #Normalize for probabilities
     total_shots = sum(counts.values())
     probabilities = {state: count / total_shots for state, count in counts.items()}
-
 #Construct measured density matrix: Note needs to remain hardcoded for 2 qubit system for current backend config
     num_states = 4 
     measured_density_matrix = np.zeros((num_states, num_states), dtype=complex)
@@ -17,10 +16,102 @@ def calculate_fidelity(counts, ideal_density_matrix):
         outer_product = np.outer(np.array([1 if j == i else 0 for j in range(num_states)]), 
                                  np.array([1 if j == i else 0 for j in range(num_states)]))
         measured_density_matrix += prob * outer_product
-
     #Convert to DensityMatrix object
     rho_measured = DensityMatrix(measured_density_matrix)
-
-
     fidelity = state_fidelity(rho_measured, ideal_density_matrix)
     return fidelity
+
+def calculate_fidelity( counts, ideal_density_matrix):
+#Normalize for probabilities
+    total_shots = sum(counts.values())
+    probabilities = {state: count / total_shots for state, count in counts.items()}
+#Construct measured density matrix: Note needs to remain hardcoded for 2 qubit system for current backend config
+    num_states = 4 
+    measured_density_matrix = np.zeros((num_states, num_states), dtype=complex)
+    states = ['00', '01', '10', '11']  
+    for i, state in enumerate(states):
+        prob = probabilities.get(state, 0)  # Prob for each state
+        # |state><state|
+        outer_product = np.outer(np.array([1 if j == i else 0 for j in range(num_states)]), 
+                                 np.array([1 if j == i else 0 for j in range(num_states)]))
+        measured_density_matrix += prob * outer_product
+    #Convert to DensityMatrix object
+    rho_measured = DensityMatrix(measured_density_matrix)
+    fidelity = state_fidelity(rho_measured, ideal_density_matrix)
+    return fidelity
+
+def fidelity_circ_v1(pulse_params):
+    test=pulse_circ(*pulse_params)
+    required_keys = ['00', '01', '10', '11']
+    for key in required_keys:
+        test.setdefault(key, 0)
+    counts = {'00': int(test['00']), '01': int(test['01']), '10': int(test['10']), '11': int(test['11'])} 
+    total_shots = sum(counts.values())
+    probabilities = {state: count / total_shots for state, count in counts.items()}
+    num_states = 4 
+    measured_density_matrix = np.zeros((num_states, num_states), dtype=complex)
+    states = ['00', '01', '10', '11']  
+    for i, state in enumerate(states):
+        prob = probabilities.get(state, 0)  # Prob for each state
+        # |state><state|
+        outer_product = np.outer(np.array([1 if j == i else 0 for j in range(num_states)]), 
+                                 np.array([1 if j == i else 0 for j in range(num_states)]))
+        measured_density_matrix += prob * outer_product
+    #Convert to DensityMatrix object
+    rho_measured = DensityMatrix(measured_density_matrix)#measured_density_matrix#DensityMatrix(measured_density_matrix)
+
+    fidelity = state_fidelity(rho_measured, des_state,validate=False)
+    infid=1-np.abs(fidelity) 
+    infid_dat.append(infid)
+    print('infidelity: '+str(infid) )
+    return infid 
+
+def calculate_fidelity_circ_2s(pulse_params):
+    test=pulse_circ(*pulse_params)
+    required_keys = ['0', '1']
+    for key in required_keys:
+        test.setdefault(key, 0)
+    counts = {'0': int(test['0']), '1': int(test['1'])} 
+    total_shots = sum(counts.values())
+    probabilities = {state: count / total_shots for state, count in counts.items()}
+    num_states = 2 
+    measured_density_matrix = np.zeros((num_states, num_states), dtype=complex)
+    states = ['0','1']  
+    for i, state in enumerate(states):
+        prob = probabilities.get(state, 0)  # Prob for each state
+        # |state><state|
+        outer_product = np.outer(np.array([1 if j == i else 0 for j in range(num_states)]), 
+                                 np.array([1 if j == i else 0 for j in range(num_states)]))
+        measured_density_matrix += prob * outer_product
+    #Convert to DensityMatrix object
+    rho_measured = DensityMatrix(measured_density_matrix)#measured_density_matrix#DensityMatrix(measured_density_matrix)
+    fidelity = state_fidelity(rho_measured, des_state,validate=False)
+    infid=1-np.abs(fidelity) 
+    #infid_dat.append(infid)
+    print('infidelity: '+str(infid))
+    return infid 
+
+def calculate_fidelity_circ_4s(pulse_params):
+    test=pulse_circ_2q(*pulse_params)
+    required_keys = ['00', '01', '10', '11']
+    for key in required_keys:
+        test.setdefault(key, 0)
+    counts = {'00': int(test['00']), '01': int(test['01']), '10': int(test['10']), '11': int(test['11'])} 
+    total_shots = sum(counts.values())
+    probabilities = {state: count / total_shots for state, count in counts.items()}
+    num_states = 4 
+    measured_density_matrix = np.zeros((num_states, num_states), dtype=complex)
+    states = ['00', '01', '10', '11']  
+    for i, state in enumerate(states):
+        prob = probabilities.get(state, 0)  # Prob for each state
+        # |state><state|
+        outer_product = np.outer(np.array([1 if j == i else 0 for j in range(num_states)]), 
+                                 np.array([1 if j == i else 0 for j in range(num_states)]))
+        measured_density_matrix += prob * outer_product
+    #Convert to DensityMatrix object
+    rho_measured = DensityMatrix(measured_density_matrix)#measured_density_matrix#DensityMatrix(measured_density_matrix)
+    fidelity = state_fidelity(rho_measured, des_state,validate=False)
+    infid=1-np.abs(fidelity) 
+    #infid_dat.append(infid)
+    print('infidelity: '+str(infid) )
+    return infid
--- pulses.py
+++ pulses.py
@@ -4,8 +4,21 @@ from pkg_req import *
 #amp: The amplitude of the Gaussian pulse.
 #sigma: The standard deviation (width) of the Gaussian pulse.
 
+def Ry10():
+        pulse.play(pulse.library.Gaussian(4386, 0.7, 1000), pulse.DriveChannel(0))
+        pulse.acquire(duration=1000, qubit_or_channel=0, register=pulse.MemorySlot(0))
+def Ry01():
+        pulse.play(pulse.library.Gaussian(3000, 0.22,1000), pulse.DriveChannel(1))
+        pulse.acquire(duration=1000, qubit_or_channel=1, register=pulse.MemorySlot(0))
+def Ry11():
+        pulse.play(pulse.library.Gaussian(4386, 0.7, 1000), pulse.DriveChannel(0))
+        #pulse.acquire(duration=1000, qubit_or_channel=0, register=pulse.MemorySlot(0))
+        pulse.play(pulse.library.Gaussian(3000, 0.22,1000), pulse.DriveChannel(1))
+        #pulse.acquire(duration=1000, qubit_or_channel=1, register=pulse.MemorySlot(0))
+        #pulse.measure_all()
+
 def gauss(num_samples_g, amp_g, sigma_g, angle_g):
-    gauss = pulse.library.Gaussian(duration=num_samples_g, amp=amp_g, sigma=sigma_g, angle=angle_g)
+    gauss = pulse.library.Gaussian(duration=num_samples_g, amp=amp_g, sigma=sigma_g, angle=angle_g,limit_amplitude=False)
     return gauss
 
 
@@ -26,6 +39,11 @@ def gauss_CR(dur_CR, amp_CR, sigma_CR, angle_CR, width_CR):
         active_amp=0.0, 
         active_angle=0.0, 
         risefall_sigma_ratio=None, 
-        limit_amplitude=None
+        limit_amplitude=False
     )
     return gauss_sq_echo
+
+def x180(dur_x, amp_x, sigma_x, angle_x, beta_x):
+    # Correct the order: keyword arguments should follow positional ones
+    x180=pulse.library.Drag(duration=dur_x, amp=amp_x, sigma=sigma_x, angle=angle_x,beta=beta_x,limit_amplitude=False) 
+    return x180
